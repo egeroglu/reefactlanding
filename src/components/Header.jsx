ff2fcd6ded6078edbe8c2bfe99dc8857dadd7680
Erdem Ege Eroğlu <erdemegeeroglu@gmail.com>
2024-03-17 16:59:29 +0300

    header fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,6 @@
+
+
+
 import { useLocation } from "react-router-dom";
 import { disablePageScroll, enablePageScroll } from "scroll-lock";
 
@@ -46,11 +49,29 @@ const Header = () => {
         <nav
           className={`${
             openNavigation ? "flex" : "hidden"
-          }  bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
+          } fixed top-[5rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
         >
+          <div className="relative z-2 flex flex-col items-center justify-center m-auto md:hidden">
+            {navigation.map((item) => (
+              <a
+                key={item.id}
+                href={item.url}
+                onClick={handleClick}
+                className={`block relative font-code text-2xl uppercase text-n-1 transition-colors hover:text-color-1 px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
+                  item.url === pathname.hash
+                    ? "z-2 lg:text-n-1"
+                    : "lg:text-n-1/50"
+                } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
+              >
+                {item.title}
+              </a>
+            ))}
+          </div>
+
 
           <HamburgerMenu />
         </nav>
+
         <a
           href="#features"
           className="button hidden mr-16 text-n-1/50 transition-colors hover:text-n-1 lg:block"
